b3d007eceafbaaa23b2a984b183a96a7bd75a2dc
Jaller123 <jallerken@hotmail.se>
2025-08-20 18:52:37 +0200

    added navbat temp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import InputAI from '../components/InputAI';
+import Navbar from '../components/Navbar';
 
 function App() {
   return (
     <>
-   <InputAI />
+      <Navbar />
+      <InputAI />
     </>
   )
 }
